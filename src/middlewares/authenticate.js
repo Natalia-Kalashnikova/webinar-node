80394b0cb981671141c46efbac8cfe00f896aac2
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-08-21 02:52:54 +0300

    Add authenticate

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,42 +1,42 @@
-// **WEBINAR-CODE**
+// **WEBINAR-CODE** 5(2) 00:50
 
-// import createHttpError from "http-errors";
-// import { Session } from "../db/models/session.js";
-// import { User } from "../db/models/user.js";
+import createHttpError from "http-errors";
+import { Session } from "../db/models/session.js";
+import { User } from "../db/models/user.js";
 
 
-// export const authenticate = async (req, res, next) => {
-//   const header = req.get('Authorization');
+export const authenticate = async (req, res, next) => {
+  const header = req.get('Authorization');
 
-//   if (!header) {
-//     return next(createHttpError(401, 'Auth header is not provided'));
-//   }
+  if (!header) {
+    return next(createHttpError(401, 'Auth header is not provided'));
+  }
 
-//   const [bearer, token] = header.split(' ');
+  const [bearer, token] = header.split(' ');
 
-//   if (bearer !== 'Bearer' || !token) {
-//     return next(createHttpError(401, 'Auth header should be of bearer type'));
-//   }
+  if (bearer !== 'Bearer' || !token) {
+    return next(createHttpError(401, 'Auth header should be of bearer type'));
+  }
 
-//   const session = await Session.findOne({ accessToken: token });
+  const session = await Session.findOne({ accessToken: token });
 
-//   if (!session) {
-//     return next(createHttpError(401, 'Session not found!'));
-//   }
+  if (!session) {
+    return next(createHttpError(401, 'Session not found!'));
+  }
 
-//   if (Date.now() > session.accessTokenValidUntil) {
-//     return next(createHttpError(401, 'Session token is expired!'));
-//   }
+  if (Date.now() > session.accessTokenValidUntil) {
+    return next(createHttpError(401, 'Session token is expired!'));
+  }
 
-//   const user = await User.findById(session.userId);
+  const user = await User.findById(session.userId);
 
-//   if (!user) {
-//     return next(
-//       createHttpError(401, 'User associated with this session is not found!'),
-//     );
-//   }
+  if (!user) {
+    return next(
+      createHttpError(401, 'User associated with this session is not found!'),
+    );
+  }
 
-//   req.user = user;
+  req.user = user;
 
-//   return next();
-// };
+  return next();
+};
