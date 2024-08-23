3f44c54d8d6ae4e86a3779ec4e8ec63e5428697c
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-08-24 01:37:42 +0300

    Update index

--- src/constants/index.js
+++ src/constants/index.js
@@ -31,3 +31,4 @@ export const ENV_VARS = {
 
 export const TEMPLATE_DIR = path.join(process.cwd(), 'src', 'templates');
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
+export const UPLOAD_DIR = path.join(process.cwd(), 'upload');
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 // **WEBINAR-CODE* 5-2
+// import { initMongoConnection } from './db/initMongoConnection.js';
+// import { startServer } from './server.js';
+
+// (async () => {
+//   await initMongoConnection();
+//   startServer();
+// })();
+
+// **WEBINAR-CODE* 6
+
+import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { startServer } from './server.js';
+import { createFolderIfDoesNotExist } from './utils/createDirIfNotExists.js';
 
 (async () => {
   await initMongoConnection();
+  await createFolderIfDoesNotExist(TEMP_UPLOAD_DIR);
+  await createFolderIfDoesNotExist(UPLOAD_DIR);
   startServer();
 })();
