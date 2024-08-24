d822bb2ab87132f1a1521dd7d98455ba2905f3b8
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-08-25 00:34:44 +0300

    Add upload local

--- src/index.js
+++ src/index.js
@@ -10,13 +10,14 @@
 // **WEBINAR-CODE* 6
 
 // import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
+import { TEMP_UPLOAD_DIR } from './constants/index.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { startServer } from './server.js';
-// import { createFolderIfDoesNotExist } from './utils/createDirIfNotExists.js';
+import { createFolderIfDoesNotExist } from './utils/createDirIfNotExists.js';
 
 (async () => {
   await initMongoConnection();
-  // await createFolderIfDoesNotExist(TEMP_UPLOAD_DIR);
+  await createFolderIfDoesNotExist(TEMP_UPLOAD_DIR);
   // await createFolderIfDoesNotExist(UPLOAD_DIR);
   startServer();
 })();
--- src/utils/createDirIfNotExists.js
+++ src/utils/createDirIfNotExists.js
@@ -1,12 +1,13 @@
 // **WEBINAR-CODE* 6-2
-// import fs from 'node:fs/promises';
+// 00:55
+import fs from 'node:fs/promises';
 
-// export const createFolderIfDoesNotExist = async (path) => {
-//   try {
-//     await fs.access(path);
-//   } catch (err) {
-//     if (err.code === 'ENOENT') {
-//       await fs.mkdir(path);
-//     }
-//   }
-// };
+export const createFolderIfDoesNotExist = async (path) => {
+  try {
+    await fs.access(path);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      await fs.mkdir(path);
+    }
+  }
+};
