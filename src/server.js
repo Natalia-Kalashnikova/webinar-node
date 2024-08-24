88fc6189841d28d3d6b38971d7c361368a3acba2
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-08-24 03:41:33 +0300

    Add storage

--- src/server.js
+++ src/server.js
@@ -4,10 +4,11 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-import { ENV_VARS } from './constants/index.js';
+import { ENV_VARS, UPLOAD_DIR } from './constants/index.js';
 import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware.js';
 import rootRouter from './routers/index.js';
 import cookieParser from 'cookie-parser';
+import { notFoundMiddleware } from './middlewares/notFoundMiddleware.js';
 
 export const startServer = () => {
   const app = express();
@@ -28,8 +29,11 @@ export const startServer = () => {
     type: ['application/json', 'application/vnd.api+json'],
   }));
 
+  app.use('/uploads', express.static(UPLOAD_DIR));
+
   app.use(rootRouter);
 
+  app.use(notFoundMiddleware);
 
   app.use(errorHandlerMiddleware);
 
