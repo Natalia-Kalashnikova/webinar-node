ceef227328633bb19d960fd2f7ba690832e59c1d
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-08-25 03:26:47 +0300

    Update

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -1,92 +1,3 @@
-// **WEBINAR-CODE* 5-2
-
-// import {
-//     createStudent,
-//     deleteStudentById,
-//     getAllStudents,
-//     getStudentById,
-//     upsertStudent
-// } from "../services/students.js";
-// import { parseFilterParams } from "../utils/parseFilterParams.js";
-// import { parsePaginationParams } from "../utils/parsePaginationParams.js";
-
-
-// export const getStudentsController = async (req, res) => {
-//     const { page, perPage } = parsePaginationParams(req.query);
-//     const { sortBy, sortOrder } = req.query;
-//     const filter = parseFilterParams(req.query);
-
-//     const students = await getAllStudents({
-//         page,
-//         perPage,
-//         sortBy,
-//         sortOrder,
-//         filter,
-//         userId: req.user._id,
-//     });
-
-//     res.json({
-//         status: 200,
-//         message: 'Successfully get all students!',
-//         data: students,
-//     });
-// };
-
-
-// export const getStudentByIdController = async (req, res) => {
-//     const id = req.params.studentId;
-//     const student = await getStudentById(id);
-//     res.json({
-//         status: 200,
-//         message: `Successfully get student with id ${id}!`,
-//         data: student,
-//     });
-// };
-
-// export const createStudentController = async (req, res) => {
-//     const {body} = req;
-//     const student = await createStudent(body, req.user._id);
-
-//     res.status(201).json({
-//         status: 201,
-//         message: `Successfully created student`,
-//         data: student,
-//     });
-// };
-
-// export const patchStudentController = async (req, res) => {
-//     const { body } = req;
-//     const {studentId} = req.params;
-//     const student = await upsertStudent(studentId, body);
-
-//     res.status(200).json({
-//         status: 200,
-//         message: `Successfully patched student`,
-//         data: student,
-//     });
-// };
-
-// export const putStudentController = async (req, res) => {
-//     const { body } = req;
-//     const {studentId} = req.params;
-//     const { isNew, student } = await upsertStudent(studentId, body, {
-//         upsert: true
-//     });
-
-//     const status = isNew ? 201 : 200;
-//     res.status(status).json({
-//         status,
-//         message: `Successfully upserted student`,
-//         data: student,
-//     });
-// };
-
-// export const deleteStudentByIdController = async (req, res) => {
-//     const id = req.params.studentId;
-//     await deleteStudentById(id);
-//     res.status(204).send();
-// };
-
 // **WEBINAR-CODE* 6
 // 00:58
 
@@ -145,9 +56,12 @@ export const createStudentController = async (req, res) => {
 };
 
 export const patchStudentController = async (req, res) => {
-    const { body } = req;
+    const { body, file } = req;
     const {studentId} = req.params;
-    const student = await upsertStudent(studentId, body);
+    const student = await upsertStudent(
+        studentId,
+        { ...body, avatar: file }
+    );
 
     res.status(200).json({
         status: 200,
@@ -157,11 +71,15 @@ export const patchStudentController = async (req, res) => {
 };
 
 export const putStudentController = async (req, res) => {
-    const { body } = req;
+    const { body, file } = req;
     const {studentId} = req.params;
-    const { isNew, student } = await upsertStudent(studentId, body, {
+    const { isNew, student } = await upsertStudent(
+        studentId,
+        { ...body, avatar: file },
+        {
         upsert: true
-    });
+    }
+);
 
     const status = isNew ? 201 : 200;
     res.status(status).json({
--- src/routers/students.js
+++ src/routers/students.js
@@ -1,58 +1,3 @@
-// **WEBINAR-CODE* 5-2
-
-// import { Router } from "express";
-// import {
-//     createStudentController,
-//     deleteStudentByIdController,
-//     getStudentByIdController,
-//     getStudentsController,
-//     patchStudentController,
-//     putStudentController
-// } from "../controllers/students.js";
-// import { ctrWrapper } from "../middlewares/ctrlWrapper.js";
-// import { isValidId } from "../middlewares/isValidId.js";
-// import { validateBody } from "../middlewares/validateBody.js";
-// import { createStudentSchema } from "../validation/createStudentSchema.js";
-// import { updateStudentSchema } from "../validation/updateStudentSchema.js";
-// import { authenticate } from "../middlewares/authenticate.js";
-// import { checkChildPermissions } from "../middlewares/checkRoles.js";
-
-
-// const studentsRouter = Router();
-
-// studentsRouter.use('/:studentId', isValidId('studentId'));
-
-// studentsRouter.use('/', authenticate);
-
-// studentsRouter.get('/', ctrWrapper(getStudentsController));
-
-// studentsRouter.get('/:studentId', ctrWrapper(getStudentByIdController));
-
-// studentsRouter.post(
-//     '/',
-//     validateBody(createStudentSchema),
-//     ctrWrapper(createStudentController)
-// );
-
-// studentsRouter.patch(
-//     '/:studentId',
-//     checkChildPermissions('teacher', 'parent'),
-//     validateBody(updateStudentSchema),
-//     ctrWrapper(patchStudentController)
-// );
-
-// studentsRouter.put(
-//     '/:studentId',
-//     validateBody(createStudentSchema),
-//     ctrWrapper(putStudentController)
-// );
-
-// studentsRouter.delete('/:studentId', ctrWrapper(deleteStudentByIdController));
-
-
-// export default studentsRouter;
-
-
 // **WEBINAR-CODE* 6
 
 import { Router } from "express";
@@ -95,12 +40,14 @@ studentsRouter.post(
 studentsRouter.patch(
     '/:studentId',
     checkChildPermissions('teacher', 'parent'),
+    upload.single('avatar'),
     validateBody(updateStudentSchema),
     ctrWrapper(patchStudentController)
 );
 
 studentsRouter.put(
     '/:studentId',
+    upload.single('avatar'),
     validateBody(createStudentSchema),
     ctrWrapper(putStudentController)
 );
